3589383481d52845a24b8d90f1298c912aa58610
UtkuDenizSonmez <73853487+UtkuDenizSonmez@users.noreply.github.com>
2021-06-12 19:13:00 +0300

    remove dbName and Pass

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-utku:test123@cluster0.safle.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect("mongodb+srv://<dbName>:<password>@cluster0.safle.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 const itemsSchema = new mongoose.Schema({
   name: String,
